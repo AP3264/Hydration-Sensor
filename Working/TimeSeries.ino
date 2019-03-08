326c2883d6b4449568001dbfa2e472d51d0d6ba1
AP3264 <anav.pr999@gmail.com>
2019-03-08 14:06:42 -0500

    final winter update

--- Working/TimeSeries.ino
+++ Working/TimeSeries.ino
@@ -1,6 +1,12 @@
+//Hydration Sensor
+//Drexel University - Senior Design Team 18
+//Anav Pradhan, Anishi Patel, Tyler Harris, Nicholas Joseph, Anishi Patel
+
 #include <LinkedList.h>
 #include <Wire.h>
+#include <Adafruit_ADS1015.h>
 
+//Initialize required variables
 LinkedList<int> valList670;
 LinkedList<int> valList850;
 LinkedList<int> valList950;
@@ -12,125 +18,79 @@ unsigned short Count;
 unsigned short Count2;
 unsigned short Count3;
 unsigned long timer;
+int result;
+
+//Setup ADC
+Adafruit_ADS1015 adc;
+int adc0;
+int adc1;
+int adc2;
+
 #define ledPin670 13
 #define ledPin850 12
 #define ledPin950 11
-#define analogIn A2
+
+//readSPeed = rate at which individual samples are taken
+#define readSpeed 5
+
+//Cycletime = Time for which each LED is on, PauseTime = Interval time while switching LEDs, TotalTime = Time of entire cycle
+#define CycleTime 100
+#define PauseTime 10
+#define TotalTime 500
+
+#define mosfetPin 9
+#define mosfetPin_2 8
+#define mosfetPin_3 7
+
 
 void setup( void )
 {
   Serial.begin( 38400 );
-  while(!Serial){
-    }
+  while(!Serial){}
   Wire.begin();
+  adc.begin();
+
+//  while(!Serial.available());
+//  result = Serial.read();
+  
+  
   pinMode (ledPin670, OUTPUT);
   pinMode (ledPin850, OUTPUT);
   pinMode (ledPin950, OUTPUT);
+  pinMode(mosfetPin, OUTPUT);
+  pinMode(mosfetPin_2, OUTPUT);
+  pinMode(mosfetPin_3, OUTPUT);
+  
   digitalWrite(ledPin670,HIGH);
   digitalWrite(ledPin850,HIGH);
   digitalWrite(ledPin950,HIGH);
+  
   Start = millis();
   timer = micros();
 }
 
-void loop( void )
+//Initializes and sends potentiometer value
+void set_pot_val(int byteVal)
 {
-  Wire.beginTransmission(44);
-  
-  if ( millis() - Start < 100 )
-  {
+    Wire.beginTransmission(44);
     Wire.write(byte(0x00));
-    Wire.write(byte(21));
+    Wire.write(byte(byteVal));
     Wire.endTransmission();
-    //Serial.read();
-    if (Count == 0){
-    Serial.println(" ");
-    }
-    delay(5);
-    digitalWrite(ledPin670,LOW);
-    analogRead( analogIn );
-    valList670.add(analogRead( analogIn ));
-    time670.add(micros() - timer);
-    ++Count;
-  }
-  else if ((millis() - Start >= 100) && (millis() - Start < 110)){
-    digitalWrite(ledPin670,HIGH);
-    }
-  else if ((millis() - Start >= 110) && (millis() - Start < 210))
-  {
-    Wire.write(byte(0x00));
-    Wire.write(byte(27));
-    Wire.endTransmission();
-//    Serial.read();
-    if (Count2 == 0){
-    Serial.println(" ");
-    }
-    //Serial.println(" ");
-    delay(5);
-    digitalWrite(ledPin670,HIGH);
-    digitalWrite(ledPin850,LOW);
-    analogRead( analogIn );
-    valList850.add(analogRead(analogIn));
-    time850.add(micros() - timer);
-    ++Count2;
-  }
-  else if ((millis() - Start >= 210) && (millis() - Start < 220)){
-    digitalWrite(ledPin850,HIGH);
-    }
-  else if ((millis() - Start >= 220) && (millis() - Start < 320))
-  {
-    Wire.write(byte(0x00));
-    Wire.write(byte(29));
-    Wire.endTransmission();
-    //Serial.read();
-    if (Count3 == 0){
-    Serial.println(" ");
-    }
-    delay(5);
-    digitalWrite(ledPin850,HIGH);
-    digitalWrite(ledPin950,LOW);
-    analogRead( analogIn );
-    valList950.add(analogRead( analogIn ));
-    time950.add(micros() - timer);
-    ++Count3;
-  }
-  else
-  {
-    digitalWrite(ledPin950,HIGH);
-    //Serial.println(millis() - Start);
-//    Serial.println("count:");
-//    Serial.println( Count );
-//    Serial.println( Count2 );
-//    Serial.println( Count3 );
-    arrayLoop(valList670, time670,1);
-    arrayLoop(valList850, time850,1);
-    arrayLoop(valList950, time950,1);
-
-   int listSize = valList670.size();
-   int listSize1 = valList850.size();
-   int listSize2= valList950.size();
-   //Serial.println(time670.size());
-      
-    valList670.clear();
-    valList850.clear();
-    valList950.clear();
-    time850.clear();
-    time670.clear();
-    time950.clear();
-    //timer = micros();
-    Count = 0;
-    Count2 = 0;
-    Count3 = 0;
-    Start = millis();
-    digitalWrite(ledPin670,HIGH);
-    digitalWrite(ledPin850,HIGH);
-    digitalWrite(ledPin950,HIGH);
-    
-  }
 }
 
-void arrayLoop(LinkedList<int> &pinNum,LinkedList<unsigned long> &timer, int valDiff){
-//  Serial.println(pinNum);
+//Adds current time and OPT101 reading to specified lists
+void add_to_list(LinkedList<int> &pinNum,LinkedList<unsigned long> &timeList, int analogPin)
+{
+   // pinNum.add(analogRead( analogIn ));
+    pinNum.add(adc.readADC_SingleEnded(analogPin));
+    timeList.add(micros() - timer);
+}
+
+//Print output to serial
+void print_to_ser(LinkedList<int> &pinNum,LinkedList<unsigned long> &timer, int valDiff)
+{
+//This function takes in the list of time and reading values, formats it and prints to serial
+
   int listSize = pinNum.size();
   for (int h = 0; h < listSize; h+=valDiff) {
       //Get value from list
@@ -139,8 +99,121 @@ void arrayLoop(LinkedList<int> &pinNum,LinkedList<unsigned long> &timer, int val
         unsigned long tim = timer.get(h);
         Serial.print(tim);
         Serial.print("\t");
-        Serial.println((val/1023.0)*5);
+        //Serial.println(val);
+        Serial.println((val/2048.0)*6144);
       }
   }
   Serial.println(" ");
+}
+
+void loop( void )
+{
+  //For first LED
+  if ( millis() - Start < CycleTime )
+  {
+    set_pot_val(21);
+    digitalWrite(mosfetPin,HIGH);
+
+    if (Count == 0)
+    {
+      Serial.println(" ");
+    }
+    delay(readSpeed);
+    digitalWrite(ledPin670,LOW);
+    adc.readADC_SingleEnded(0);
+    add_to_list(valList670, time670,0);
+    ++Count;
+  }
+
+  //First interval between LED switch
+  else if ((millis() - Start >= CycleTime) && (millis() - Start < (CycleTime + PauseTime)))
+  {
+    digitalWrite(ledPin670,HIGH);
+    digitalWrite(mosfetPin,LOW);
+  }
+
+  //For second LED  
+  else if ((millis() - Start >= (CycleTime+PauseTime)) && (millis() - Start < (2*CycleTime+PauseTime)))
+  {
+    set_pot_val(27);
+    digitalWrite(mosfetPin_2,HIGH);
+
+    if (Count2 == 0){
+    Serial.println(" ");
+    }
+
+    delay(readSpeed);
+    digitalWrite(ledPin670,HIGH);
+    digitalWrite(ledPin850,LOW);
+    adc.readADC_SingleEnded(1);
+    
+    add_to_list(valList850, time850,1);
+    
+    ++Count2;
+  }
+
+  //interval between LED switch
+  else if ((millis() - Start) >= ((2*CycleTime)+(PauseTime)) && (millis() - Start) < ((2*CycleTime)+(2*PauseTime)))
+  {
+    digitalWrite(ledPin850,HIGH);
+    digitalWrite(mosfetPin_2,LOW);
+  }
+  
+  //For third LED  
+  else if ((millis() - Start >= ((2*CycleTime)+(2*PauseTime))) && (millis() - Start < ((3*CycleTime)+(2*PauseTime))))
+  {
+    set_pot_val(29);
+    digitalWrite(mosfetPin_3,HIGH);
+    
+    if (Count3 == 0)
+    {
+    Serial.println(" ");
+    }
+    
+    delay(readSpeed);
+    digitalWrite(ledPin850,HIGH);
+    digitalWrite(ledPin950,LOW);
+    adc.readADC_SingleEnded(2);
+    
+    add_to_list(valList950, time950,2);
+    ++Count3;
+  }
+
+  //Process collected data, reset all values, reset timer
+  else
+  {
+    digitalWrite(ledPin950,HIGH);
+    digitalWrite(mosfetPin_3,LOW);
+    
+    //Print all values to serial port
+    print_to_ser(valList670, time670,1);
+    print_to_ser(valList850, time850,1);
+    print_to_ser(valList950, time950,1);
+
+    //Reset all list to empty
+    valList670.clear();
+    valList850.clear();
+    valList950.clear();
+    time850.clear();
+    time670.clear();
+    time950.clear();
+    
+    //Reset Counters to 0 and restart timer
+    while ((millis()-Start)<=TotalTime){     
+      if ((millis()-Start)>TotalTime){
+        break;
+        }
+    }
+    
+    Count = 0;
+    Count2 = 0;
+    Count3 = 0;
+    Start = millis();
+
+    //Ensure all LEDs off
+    digitalWrite(ledPin670,HIGH);
+    digitalWrite(ledPin850,HIGH);
+    digitalWrite(ledPin950,HIGH);
+    }
+    
 }
